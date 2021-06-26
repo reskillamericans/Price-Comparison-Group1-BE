82bfb715fbe20e9459dc395a8b6d296ee959b1f6
EHoggard <ebonnee.nelson@gmail.com>
2021-06-26 06:16:02 -0400

    Amazon

--- price_comparison_gp1/urls.py
+++ price_comparison_gp1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import index
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 ]
