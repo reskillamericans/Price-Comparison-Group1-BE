7ce096ba64c9fbdfb75c3ce621e126cd712bdb38
Jalen Harris <Harrisj22412@hocking.edu>
2021-07-05 08:58:46 -0700

    Add files via upload

--- products/models.py
+++ products/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# Create your models here.
 
-class Product(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.TextField()
-    date_created = models.DateTimeField(auto_now_add= True)
-    date_updated = models.DateTimeField(auto_now_add= True)
+class LikeButton(models.Model):
+    content=models.TextField(null=True)
+    likes = models.ManyToManyField(User, blank=True, related_name= 'likes')
 
-    def __str__(self):
-        return self.name
+    @property
+    def total_likes(self):
+        return self.likes.count()
