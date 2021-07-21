15098eca79f8d0402ddc11ffa2174d6f8196d026
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-07-21 15:17:26 +0100

    fixes

--- price_comparison_gp1/settings.py
+++ price_comparison_gp1/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -152,6 +153,8 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'emails')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_on_heroku.settings(locals())
