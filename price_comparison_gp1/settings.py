0b89bbc1f664f5c9925f51ad5d4b77e7ed9de6b2
dmking006 <devonking001@gmail.com>
2021-07-21 23:23:26 -0400

    Various fixes: - Use Cloudinary to store images - Css for responsiveness - Changed the way images are set - Added update all for Edit Products

--- price_comparison_gp1/settings.py
+++ price_comparison_gp1/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-obfoys!+la)x3kprc(ls5df0i@5q$6mm_oh280ufw3m6&3-^th'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
