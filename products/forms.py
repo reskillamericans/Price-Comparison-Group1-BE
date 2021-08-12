32d8f9fad26d128cd73ee5a85750cbf1d34fa510
dmking006 <devonking001@gmail.com>
2021-08-12 14:06:10 -0400

    Some css fixes

--- products/forms.py
+++ products/forms.py
@@ -5,8 +5,15 @@ class AddProductForm(forms.Form):
     """
     Adds products. Used with add product view and template
     """
-    amazon_asin = forms.CharField(max_length=12)
-    ebay_url = forms.CharField(max_length=200)
+    amazon_asin = forms.CharField(max_length=12,
+                                  widget=forms.TextInput(attrs={
+                                      'autofocus': 'True',
+                                      'placeholder': 'Bxxxxxxxxx'
+                                      }))
+    ebay_url = forms.CharField(max_length=200,
+                               widget=forms.TextInput(attrs={
+                                   'placeholder': 'https://www.ebay.com/itm/xxxxxxxxxxxx'
+                                   }))
 
 
 class ContactUsForm(forms.Form):
@@ -14,4 +21,4 @@ class ContactUsForm(forms.Form):
     Form for contact us.
     """
     email = forms. EmailField()
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}))
